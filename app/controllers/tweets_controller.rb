b653d0cd01b3c05a4d60a6b4fa27cb3d54d7db07
Bomtemmaru <reotang0405@gmail.com>
2021-10-28 19:28:23 +0900

    viewの変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     # @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -31,6 +32,11 @@ class TweetsController < ApplicationController
   def show
   end
 
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+
+
   private
 
   def tweet_params
@@ -39,6 +45,12 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
